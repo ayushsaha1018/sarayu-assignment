cd43ba45a48bb9a817aa7ff491fbb5d99c27e6f4
ayushsaha1018 <ayushsaha111@gmail.com>
2024-09-21 15:13:34 +0530

    added rate limiter

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,12 @@
+import cors from "cors";
 import "dotenv/config";
 import express from "express";
+import rateLimit from "express-rate-limit";
 import helmet from "helmet";
-import cors from "cors";
 
 // routers
-import userRouter from "./routes/user.routes";
 import eventRouter from "./routes/event.routes";
+import userRouter from "./routes/user.routes";
 
 const app = express();
 
@@ -13,6 +14,16 @@ app.use(cors());
 app.use(helmet());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+});
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter);
 
 app.get("/", (req, res) => {
   return res.send("API is working....");
